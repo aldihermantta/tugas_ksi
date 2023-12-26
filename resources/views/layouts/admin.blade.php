8ac31db41262439b91312740a6ad1cc13947d8eb
rizky <rizkydwi603@gmail.com>
2023-12-26 10:36:37 +0700

    perapian lagi

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -17,30 +17,30 @@
 </head>
 
 <body>
-    <div id="app">
+     <div id="app">
           <div id="sidebar" class="active">
                <div class="sidebar-wrapper active">
                     @include('partials.topbar')
                     {{-- ----------------------- --}}
                     @include('partials.sidebar')
                </div>
-        </div>
-        <div id="main">
+          </div>
+          <div id="main">
                {{-- <header class="mb-3">
                     <a href="#" class="burger-btn d-block d-xl-none">
                          <i class="bi bi-justify fs-3">ineofio</i>
                     </a>
                </header> --}}
-            
-          <div class="page-heading">
-               <h3>Profile Statistics</h3>
-          </div>
 
-          <div class="page-content">
-               @yield('content')
-          </div>
+               <div class="page-heading">
+                    <h3>Profile Statistics</h3>
+               </div>
 
-            @include('partials.footer')
+               <div class="page-content">
+                    @yield('content')
+               </div>
+
+               @include('partials.footer')
           </div>
      </div>
      <script src="{{ asset('Assets/dist/vendors/perfect-scrollbar/perfect-scrollbar.min.js') }}"></script>
