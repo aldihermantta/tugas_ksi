0e85f33b3239e0bae11410607053c35b7effe32c
rizky <rizkydwi603@gmail.com>
2023-12-28 22:16:40 +0700

    tambahan khusus blog

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1081,54 +1081,73 @@
                     <h1 class="display-5 mb-5">Informasi Alun Alun</h1>
                 </div>
                 <div class="row gx-4 justify-content-center">
-                    <div class="col-md-6 col-lg-4 wow bounceInUp" data-wow-delay="0.1s">
-                        <div class="blog-item">
-                            <div class="overflow-hidden rounded">
-                                <img src="{{ asset('Assets/img/blog-1.jpg') }}" class="img-fluid w-100" alt="">
-                            </div>
-                            <div class="blog-content mx-4 d-flex rounded bg-light">
-                                <div class="text-dark bg-primary rounded-start">
-                                    <div class="h-100 p-3 d-flex flex-column justify-content-center text-center">
-                                        <p class="fw-bold mb-0">16</p>
-                                        <p class="fw-bold mb-0">Sep</p>
-                                    </div>
+                    @forelse ($artikel as $list)
+                        <div class="col-md-6 col-lg-4 wow bounceInUp" data-wow-delay="0.1s">
+                            <div class="blog-item">
+                                <div class="overflow-hidden rounded">
+                                    <img src="{{ $list->tumblr }}" class="img-fluid w-100" alt="thumbnail_artikel">
                                 </div>
-                                <a href="#" class="h5 lh-base my-auto h-100 p-3">How to get more test in your food from</a>
+                                <div class="blog-content mx-4 d-flex rounded bg-light">
+                                    {{-- <div class="text-dark bg-primary rounded-start">
+                                        <div class="h-100 p-3 d-flex flex-column justify-content-center text-center">
+                                            <p class="fw-bold mb-0">16</p>
+                                            <p class="fw-bold mb-0">Sep</p>
+                                        </div>
+                                    </div> --}}
+                                    <a href="{{ route('artikel.show', $list->slug) }}" class="h5 lh-base my-auto h-100 p-3">{{ $list->judul }}</a>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="col-md-6 col-lg-4 wow bounceInUp" data-wow-delay="0.3s">
-                        <div class="blog-item">
-                            <div class="overflow-hidden rounded">
-                                <img src="{{ asset('Assets/img/blog-2.jpg') }}" class="img-fluid w-100" alt="">
-                            </div>
-                            <div class="blog-content mx-4 d-flex rounded bg-light">
-                                <div class="text-dark bg-primary rounded-start">
-                                    <div class="h-100 p-3 d-flex flex-column justify-content-center text-center">
-                                        <p class="fw-bold mb-0">16</p>
-                                        <p class="fw-bold mb-0">Sep</p>
-                                    </div>
+                    @empty
+                        <div class="col-md-6 col-lg-4 wow bounceInUp" data-wow-delay="0.1s">
+                            <div class="blog-item">
+                                <div class="overflow-hidden rounded">
+                                    <img src="{{ asset('Assets/img/blog-1.jpg') }}" class="img-fluid w-100" alt="">
                                 </div>
-                                <a href="#" class="h5 lh-base my-auto h-100 p-3">How to get more test in your food from</a>
+                                <div class="blog-content mx-4 d-flex rounded bg-light">
+                                    <div class="text-dark bg-primary rounded-start">
+                                        <div class="h-100 p-3 d-flex flex-column justify-content-center text-center">
+                                            <p class="fw-bold mb-0">16</p>
+                                            <p class="fw-bold mb-0">Sep</p>
+                                        </div>
+                                    </div>
+                                    <a href="#" class="h5 lh-base my-auto h-100 p-3">How to get more test in your food from</a>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="col-md-6 col-lg-4 wow bounceInUp" data-wow-delay="0.5s">
-                        <div class="blog-item">
-                            <div class="overflow-hidden rounded">
-                                <img src="{{ asset('Assets/img/blog-3.jpg') }}" class="img-fluid w-100" alt="">
-                            </div>
-                            <div class="blog-content mx-4 d-flex rounded bg-light">
-                                <div class="text-dark bg-primary rounded-start">
-                                    <div class="h-100 p-3 d-flex flex-column justify-content-center text-center">
-                                        <p class="fw-bold mb-0">16</p>
-                                        <p class="fw-bold mb-0">Sep</p>
-                                    </div>
+                        <div class="col-md-6 col-lg-4 wow bounceInUp" data-wow-delay="0.3s">
+                            <div class="blog-item">
+                                <div class="overflow-hidden rounded">
+                                    <img src="{{ asset('Assets/img/blog-2.jpg') }}" class="img-fluid w-100" alt="">
                                 </div>
-                                <a href="#" class="h5 lh-base my-auto h-100 p-3">How to get more test in your food from</a>
+                                <div class="blog-content mx-4 d-flex rounded bg-light">
+                                    <div class="text-dark bg-primary rounded-start">
+                                        <div class="h-100 p-3 d-flex flex-column justify-content-center text-center">
+                                            <p class="fw-bold mb-0">16</p>
+                                            <p class="fw-bold mb-0">Sep</p>
+                                        </div>
+                                    </div>
+                                    <a href="#" class="h5 lh-base my-auto h-100 p-3">How to get more test in your food from</a>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                        <div class="col-md-6 col-lg-4 wow bounceInUp" data-wow-delay="0.5s">
+                            <div class="blog-item">
+                                <div class="overflow-hidden rounded">
+                                    <img src="{{ asset('Assets/img/blog-3.jpg') }}" class="img-fluid w-100" alt="">
+                                </div>
+                                <div class="blog-content mx-4 d-flex rounded bg-light">
+                                    <div class="text-dark bg-primary rounded-start">
+                                        <div class="h-100 p-3 d-flex flex-column justify-content-center text-center">
+                                            <p class="fw-bold mb-0">16</p>
+                                            <p class="fw-bold mb-0">Sep</p>
+                                        </div>
+                                    </div>
+                                    <a href="#" class="h5 lh-base my-auto h-100 p-3">How to get more test in your food from</a>
+                                </div>
+                            </div>
+                        </div>
+                    @endforelse
                 </div>
             </div>
         </div>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 // Routes Khusus Landing Pages
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ArtikelController;
+use App\Http\Controllers\HomeController;
+
 
 // Routes Khusus Dashboard
 use App\Http\Controllers\Auth\AuthController;
@@ -20,27 +23,32 @@ use App\Http\Controllers\Dashboard\Artikel_Dashboard\DashboardArtikelController;
 |
 */
 
-Route::get('/', function () {
-    return view('layouts.app');
-});
+// KHUSUS LANDING PAGE
+    // Route::get('/', function () {
+    //     return view('layouts.app');
+    // });
+
+    Route::get('/', [HomeController::class, 'index']);
+
+    Route::resource('/artikels', ArtikelController::class);
 
 
-//admin
-Route::prefix('auth')->group(function () {
-    Route::get('/login', [AuthController::class, 'index'])->name('auth.login');
-    Route::post('/login', [AuthController::class, 'authenticate'])->name('auth.authenticate');
-    Route::get('/logout', [AuthController::class, 'logout'])->name('auth.logout');
-});
+// KHUSUS DASHBOARD
+    Route::prefix('auth')->group(function () {
+        Route::get('/login', [AuthController::class, 'index'])->name('auth.login');
+        Route::post('/login', [AuthController::class, 'authenticate'])->name('auth.authenticate');
+        Route::get('/logout', [AuthController::class, 'logout'])->name('auth.logout');
+    });
 
-Route::controller(DashboardController::class)->group(function () {
-    Route::get('/dashboard', 'index')->name('dashboard');
-});
+    Route::controller(DashboardController::class)->group(function () {
+        Route::get('/dashboard', 'index')->name('dashboard');
+    });
 
-// ARTIKEL
-Route::resource('dashboard/artikel', DashboardArtikelController::class, [
-    'names' => [
-        'index' => 'artikel.index',
-        'create' => 'artikel.create',
-        'store' => 'artikel.store'
-    ]
-]);
+    // ARTIKEL DASHBOARD
+    Route::resource('dashboard/artikel', DashboardArtikelController::class, [
+        'names' => [
+            'index' => 'artikel.index',
+            'create' => 'artikel.create',
+            'store' => 'artikel.store'
+        ]
+    ]);
